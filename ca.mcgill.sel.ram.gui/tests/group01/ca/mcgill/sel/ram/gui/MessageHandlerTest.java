0be64183e9e8f5bda558842e14f9e2e95f472ebf
leosiracusa <leonardo.siracusa@mail.mcgill.ca>
2015-11-20 11:56:43 -0500

    added structure

---
+++ ca.mcgill.sel.ram.gui/tests/group01/ca/mcgill/sel/ram/gui/MessageHandlerTest.java
@@ -0,0 +1,14 @@
+package group01.ca.mcgill.sel.ram.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageHandlerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
